cc01dc57d4fd599497a1a4da83c2900e3a17b5fc
amadeusz475 <amadeusz-zaradny@wp.pl>
2017-01-01 20:21:30 +0100

    dodałem rejestracje

--- header.hpp
+++ header.hpp
@@ -5,5 +5,6 @@
 //prototypy funkcji
 void czyPierwszeUruchomienie(); //sprawdza czy program ma niezbedne pliki(czy to pierwsze uruchomienie), jak nie to je tworzy
 void logowanie (); //pobiera dane do logowania i wpuszcza uzytkownika lub odrzuca
+void rejestracja (); //pobiera dane uzytkowniów i odsyła do poczekalni
 
 #endif
