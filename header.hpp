81d7a0800919925e867def7effb3cf01eeb14015
amadeusz475 <amadeusz-zaradny@wp.pl>
2017-01-01 21:06:25 +0100

    dodałem ZliczanieWierszyPliku

--- header.hpp
+++ header.hpp
@@ -6,5 +6,6 @@
 void czyPierwszeUruchomienie(); //sprawdza czy program ma niezbedne pliki(czy to pierwsze uruchomienie), jak nie to je tworzy
 void logowanie (); //pobiera dane do logowania i wpuszcza uzytkownika lub odrzuca
 void rejestracja (); //pobiera dane uzytkowniów i odsyła do poczekalni
+int ZliczanieWierszyPliku (string) //zlicza ilość wierszy pliku podanego w sciezce dostepu
 
 #endif
