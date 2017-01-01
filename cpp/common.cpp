910525683d0cf4f14f295aff74a1268a9e7a694a
amadeusz475 <amadeusz-zaradny@wp.pl>
2017-01-01 18:54:30 +0100

    dodalem logowanie

--- cpp/common.cpp
+++ cpp/common.cpp
@@ -2,6 +2,7 @@
 #include "../header.hpp"
 #include <windows.h>
 #include <fstream>
+#include <string>
 
 void czyPierwszeUruchomienie()
 {
@@ -24,5 +25,40 @@ void czyPierwszeUruchomienie()
         do_zatwierdzenia.close();
         std::ofstream ustawienia ("usr\\prof\\ustawienia.txt");
         ustawienia.close();
+    }
+}
+using namespace std;
+void logowanie ()
+{
+    int numer_wiersza=1;
+    fstream log; //plik do logowania
+    string nick , haslo;
+    string Nick_Z_Pliku , Haslo_Z_Pliku;
+    string linia; //linia pliku
+
+    cout<<"Podaj nick: "; cin>>nick;
+    cout<<"Podaj haslo: "; cin>>haslo;
+
+    log.open("hasla.txt" , ios::in);
+    if(log.good()){
+        while(getline(log,linia)){
+            switch (numer_wiersza%2)                  // wpisywanie nieparzystych linii
+            {                                         // pliku jako nicki i parzystych
+                case 1: Nick_Z_Pliku = linia; break;  // jako hasla
+                case 0: Haslo_Z_Pliku = linia; break;
+            }
+            if(nick==Nick_Z_Pliku && haslo==Haslo_Z_Pliku){
+                cout<<"zalogowany"<<endl;              // tutaj trzeba wsadzic fukcje
+                break;                                 // ktora umieszcza uzytkownika w menu itp
+            }
+            numer_wiersza++;
+        }
+
+    }
+    log.close();
+    if(numer_wiersza==ZliczanieWierszyPliku()+1)
+    {
+        cout<<"bledne haslo lub nick!!! sproboj jeszcze raz"<<endl;
+        logowanie();
     }
 }
