3da699d90172c17872ab67cb07f8b3ff8cc70cb5
Mateusz Polaszek <thepolaszek@gmail.com>
2017-01-12 16:01:04 +0100

    header, funkcje bez ;, poprawione oraz test nowej instalacji git

--- header.hpp
+++ header.hpp
@@ -6,11 +6,11 @@
 void czyPierwszeUruchomienie(); //sprawdza czy program ma niezbedne pliki(czy to pierwsze uruchomienie), jak nie to je tworzy
 void logowanie (); //pobiera dane do logowania i wpuszcza uzytkownika lub odrzuca
 void rejestracja (); //pobiera dane uzytkowniów i odsyła do poczekalni
-int ZliczanieWierszyPliku (string) //zlicza ilość wierszy pliku podanego w sciezce dostepu
-void UsunKandydata (int nr_kandydata) //Usuwa wszystkie dane o kandydacie z poczekalni
-void DopiszHaslo (int nr_kandydata) //Dopisuje nick i haslo kandydata do pliku hasla.txt
-void DrukowanieKandydatow () //wyswietla poczekalnie
-void ZatwierdzanieKont () // Pozwala na zatwierdzenie lub odrzucenie konta
+int ZliczanieWierszyPliku (string); //zlicza ilość wierszy pliku podanego w sciezce dostepu
+void UsunKandydata (int nr_kandydata); //Usuwa wszystkie dane o kandydacie z poczekalni
+void DopiszHaslo (int nr_kandydata); //Dopisuje nick i haslo kandydata do pliku hasla.txt
+void DrukowanieKandydatow (); //wyswietla poczekalnie
+void ZatwierdzanieKont (); // Pozwala na zatwierdzenie lub odrzucenie konta
 
 
 #endif
