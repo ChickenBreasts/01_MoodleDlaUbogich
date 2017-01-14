b66d9e00f63c9dfd98d21edd1ddc9f91fc3ba1d8
amadeusz475 <amadeusz-zaradny@wp.pl>
2017-01-14 13:05:22 +0100

    zalogowany profesor

--- cpp/profesor.cpp
+++ cpp/profesor.cpp
@@ -1,2 +1,26 @@
 //funkcje uzywane jedynie przez profesora
 #include "../header.hpp"
+#include <iostream>
+
+using namespace std;
+void ZalogowanyProfesor ()
+{
+    int opcja;
+    while(true){
+    cout<<"Witaj Profesorze W Moodle dla biednych!"<<endl;
+    cout<<"OPCJE:"<<endl<<"1-Poczta 2-Kursanci 3-kalendarz 4-ustawienia"<<endl;
+    cout<<"Wybierz Opcje: ";cin>>opcja;
+    switch(opcja){
+        case 1: //skrzynka();
+            break;
+        case 2: //kursanci();
+            break;
+        case 3: //kalendarz();
+            break;
+        case 4: //ustawienia();
+            break;
+        default: cout<<"Zla opcja!!! Sprobuj jeszcze raz.";break;
+        }
+    }
+
+}
